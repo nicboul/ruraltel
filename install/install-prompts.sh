62752aa9782a9760a5f6cef2e347ba2a4e5bac4f
Nicolas J. Bouliane <nicboul@gmail.com>
2015-06-19 02:47:23 +0100

    some install script

---
+++ install/install-prompts.sh
@@ -0,0 +1,4 @@
+cd ruraltel
+mv prompts/ /var/lib/asterisk/sounds/
+chown asterisk:asterisk recording/
+chown asterisk:asterisk prompts/
---
+++ install/install.sh
@@ -0,0 +1,4 @@
+apt-get install tmux
+git clone https://github.com/nicboul/ruraltel.git
+apt-get install tmux
+mkdir /var/lib/asterisk/sounds/recording
